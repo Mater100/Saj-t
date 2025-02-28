eb490412ea94949cfbc97bfd597e2cdb44b6e5f8
Lali <hegedusmate6@gmail.com>
2025-02-28 15:57:55 +0100

    Add files via upload

---
+++ resources/views/kurzusok.blade.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="hu">
+
+<head>
+    <meta charset="utf-8">
+    <title>Kurzusok</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <link href="{{ asset('img/favicon.ico') }}" rel="icon">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Nunito:wght@600;700;800&display=swap" rel="stylesheet">
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <link href="{{ asset('lib/animate/animate.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
+
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link href="css/kurzus.css" rel="stylesheet">
+</head>
+
+<body>
+    <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+        <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+            <span class="sr-only">Betöltés...</span>
+        </div>
+    </div>
+
+    <nav class="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
+        <a href="index.html" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
+            <img style="height: 50px" src="img/Új projekt.png" alt="">
+        </a>
+        <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto p-4 p-lg-0">
+                <a href="welcome" class="nav-item nav-link active">Főoldal</a>
+                <a href="rolunk" class="nav-item nav-link">Rólunk</a>
+                <a href="kurzusok" class="nav-item nav-link">Kurzusok</a>
+                <a href="oktatok" class="nav-item nav-link">Oktatók</a>
+                <a href="kapcsolat" class="nav-item nav-link">Kapcsolat</a>
+            </div>
+            <a href="bejelentkezes" class="btn btn-primary py-4 px-lg-5 d-none d-lg-block">Csatlakozz most!<i class="fa fa-arrow-right ms-3"></i></a>
+        </div>
+    </nav>
+
+    <div class="container-fluid bg-primary py-5 mb-5 page-header">
+        <div class="container py-5">
+            <div class="row justify-content-center">
+                <div class="col-lg-10 text-center">
+                    <h1 class="display-3 text-white animated slideInDown">Kurzusok</h1>
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb justify-content-center">
+                            <li class="breadcrumb-item text-white active" aria-current="page">Számos izgalmas és hasznos tanulási lehetőséget találsz, amelyek segítenek a tudásod fejlesztésében. Akár egy új készséget szeretnél elsajátítani, akár szakmai tudásodat mélyítenéd el, itt biztosan megtalálod a számodra legmegfelelőbb tanfolyamot.</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-xxl py-5 category">
+    <div class="container">
+        <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+            <h6 class="section-title bg-white text-center text-primary px-3">Kategóriák</h6>
+            <h1 class="mb-5">Kurzusok</h1>
+        </div>
+        <div>
+            <div class="col-md-6 mx-auto mb-4">
+                <input type="text" id="search" class="form-control" placeholder="Keresés kurzusok között...">
+            </div>
+        </div>
+        <div class="row">
+        @foreach ($kurzusok as $kurzus)
+        <div class="col-lg-4 col-md-6 mb-4">
+            <div class="kartya p-3 border rounded shadow-sm">
+                <div class="kartya-body">
+                    <h5 class="kartya-title">{{ $kurzus->kurzus_nev }}</h5>
+                    <p><strong>Időpont:</strong> {{ $kurzus->kepzes_ideje }}</p>
+                    <p><strong>Helyszín:</strong> {{ $kurzus->helyszin }}</p>
+                    <p><strong>Oktató:</strong> 
+                        @if ($kurzus->oktatok->isNotEmpty())
+                            {{ $kurzus->oktatok->first()->nev }}
+                        @else
+                            Nincs oktató hozzárendelve
+                        @endif
+                    </p>
+                    <p><strong>Fizetendő összeg:</strong> 
+                    @if($kurzus->dij)
+                    {{ number_format($kurzus->dij, 0, ',', ' ') }} Ft
+                        @else
+                            Nincs fizetési adat
+                        @endif
+                    </p>
+                    <form action="{{ route('kurzus.jelentkezes', $kurzus->id) }}" method="POST">
+                        @csrf
+                        <button type="submit" class="jelentkezes btn btn-primary">Jelentkezés</button>
+                    </form>
+
+                    @if(session('success'))
+                        <div class="alert alert-success mt-2">
+                            {{ session('success') }}
+                        </div>
+                    @endif
+
+                    @if(session('error'))
+                        <div class="alert alert-danger mt-2">
+                            {{ session('error') }}
+                        </div>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+    @endforeach
+    </div>
+    <div class="container-fluid bg-dark text-light footer pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s">
+        <div class="container py-5">
+            <div class="row g-5">
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="text-white mb-3">Gyors linkek</h4>
+                    <a class="btn btn-link" href="rolunk">Rólunk</a>
+                    <a class="btn btn-link" href="kapcsolat">Kapcsolatfelvétel</a>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="text-white mb-3">Elérhetőségeink</h4>
+                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>4700, Mátészalka Kölcsey utca 12</p>
+                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+36 06 12 345 678 9</p>
+                    <p class="mb-2"><i class="fa fa-envelope me-3"></i>info@acadelle.com</p>
+                    <div class="d-flex pt-2">
+                        <a class="btn btn-outline-light btn-social" href="https://x.com/?lang=en"><i class="fab fa-twitter"></i></a>
+                        <a class="btn btn-outline-light btn-social" href="https://www.facebook.com"><i class="fab fa-facebook-f"></i></a>
+                        <a class="btn btn-outline-light btn-social" href="https://www.youtube.com"><i class="fab fa-youtube"></i></a>
+                        <a class="btn btn-outline-light btn-social" href="https://hu.linkedin.com"><i class="fab fa-linkedin-in"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="text-white mb-3">Hírlevelünk</h4>
+                    <p>Értesítést küldünk minden fontos információról és érdekes újdonságokról</p>
+                    <div class="position-relative mx-auto" style="max-width: 400px;">
+                        <input class="form-control border-0 w-100 py-3 ps-4 pe-5" type="text" id="sub" placeholder="Email">
+                        <button type="button" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2" onclick="feliratkozas()">Feliratkozás</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+    <script src="js/main.js"></script>
+    <script src="js/sajat.js"></script>
+</body>
+
+</html>
---
+++ resources/views/oktatoi/oktatoi.blade.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="hu">
+
+<head>
+    <meta charset="utf-8">
+    <title>Oktatói</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <link href="{{ asset('img/favicon.ico') }}" rel="icon">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Nunito:wght@600;700;800&display=swap" rel="stylesheet">
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <link href="{{ asset('lib/animate/animate.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
+
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+        <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+    </div>
+
+    <nav class="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
+        <a href="index.html" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
+            <img style="height: 50px" src="img/Új projekt.png" alt="">
+        </a>
+        <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto p-4 p-lg-0">
+                <a href="welcome" class="nav-item nav-link active">Főoldal</a>
+                <a href="rolunk" class="nav-item nav-link">Rólunk</a>
+                <a href="kurzusok" class="nav-item nav-link">Kurzusok</a>
+                <a href="oktatok" class="nav-item nav-link">Oktatók</a>
+                <a href="kapcsolat" class="nav-item nav-link">Kapcsolat</a>
+            </div>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                @csrf
+                <button type="submit" class="btn btn-danger">Kijelentkezés</button>
+            </form>
+        </div>
+    </nav>
+
+    <div class="container-fluid bg-primary py-5 mb-5 page-header">
+        <div class="container py-5">
+            <div class="row justify-content-center">
+                <div class="col-lg-10 text-center">
+                    <h1 class="display-3 text-white animated slideInDown"></h1>
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb justify-content-center">
+                            <div class="profile-container">
+                                @if(isset($oktato))
+                                    <div class="profile-name">Üdv újra, {{ $oktato->nev }}!</div>
+                                @else
+                                    <div class="profile-name">Üdv újra, {{ Auth::guard('oktato')->user()->nev ?? 'Vendég' }}!</div>
+                                @endif
+                            </div>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <di class="user-info-container row">
+        <div class="user-details col-md">
+        @php
+            $oktato = Auth::guard('oktato')->user();
+        @endphp
+        @if(isset($oktato))
+            <h2>Felhasználó adatai</h2>
+            <p><strong>Név:</strong> {{ $oktato->nev }}</p>
+            <p><strong>Felhasználónév:</strong> {{ $oktato->felhasznalonev }}</p>
+            <p><strong>Email:</strong> {{ $oktato->email }}</p>
+        @else
+            <p class="text-center text-danger">Nincs bejelentkezett oktató!</p>
+        @endif
+        <p><strong>Tanulók listája: </strong></p>
+        <div class="diaklista">
+        @if(isset($diakok) && $diakok->isNotEmpty())
+            @foreach ($diakok as $diak)
+                <div class="filterDiv">
+                    {{ $diak->tanulo_nev }} - {{ $diak->kurzus_nev }} 
+                    (Fizetett: {{ $diak->fizetve }})
+                </div>
+            @endforeach
+        @else
+            <p>Nincs elérhető diák.</p>
+        @endif
+        </div>
+        </div>
+        <div class="courses col-md">
+        <div class="container">
+        <h3>Kurzus létrehozása</h3>
+         
+        @if(session('success'))
+            <div class="alert alert-success">{{ session('success') }}</div>
+        @endif
+        <form action="{{ route('kurzus.store') }}" method="POST">
+            @csrf
+            <div>
+                <label>Kurzus neve:</label>
+                <input type="text" name="kurzus_nev" required>
+            </div>
+            <div>
+                <label>Helyszín:</label>
+                <input type="text" name="helyszin" required>
+                <small>Írj "online"-t, ha online képzés!</small>
+            </div>
+            <div>
+                <label>Időpont:</label>
+                <input type="datetime-local" name="kepzes_ideje" required>
+            </div>
+            <div>
+                <label>Ár:</label>
+                <input type="number" name="dij">
+            </div>
+
+            <button type="submit">Létrehozás</button>
+        </form>
+        </div>
+    </div>
+        <div class="courses col-md kurzuslista">
+            <h3>Kurzusok listája: </h3>
+            <ul>
+            @if(isset($kurzusok) && $kurzusok->isNotEmpty())
+                @foreach($kurzusok as $kurzus)
+                    <li>{{ $kurzus->kurzus_nev }}</li>
+                @endforeach
+            </ul>
+            @endif
+        </div>
+    </div>
+
+    <div class="container-fluid bg-dark text-light footer pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s">
+        <div class="container py-5">
+            <div class="row g-5">
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="text-white mb-3">Gyors linkek</h4>
+                    <a class="btn btn-link" href="rolunk">Rólunk</a>
+                    <a class="btn btn-link" href="kapcsolat">Kapcsolatfelvétel</a>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="text-white mb-3">Elérhetőségeink</h4>
+                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>4700, Mátészalka Kölcsey Út 12</p>
+                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+36 06 12 345 678 9</p>
+                    <p class="mb-2"><i class="fa fa-envelope me-3"></i>info@acadelle.com</p>
+                    <div class="d-flex pt-2">
+                        <a class="btn btn-outline-light btn-social" href="https://www.facebook.com/?locale=hu_HU"><i class="fab fa-facebook-f"></i></a>
+                        <a class="btn btn-outline-light btn-social" href="https://www.youtube.com"><i class="fab fa-youtube"></i></a>
+                        <a class="btn btn-outline-light btn-social" href="https://hu.linkedin.com"><i class="fab fa-linkedin-in"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="text-white mb-3">Hírlevelünk</h4>
+                    <p>Értesítést küldünk minden fontos információról és érdekes újdonságokról</p>
+                    <div class="position-relative mx-auto" style="max-width: 400px;">
+                        <input class="form-control border-0 w-100 py-3 ps-4 pe-5" type="text" placeholder="email">
+                        <button type="button" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">Feliratkozás</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+    <script src="js/main.js"></script>
+    <script src="js/sajat.js"></script>
+</body>
+
+</html>
